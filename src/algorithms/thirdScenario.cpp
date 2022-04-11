39ddb6d74ea9a1196c238db32f2985ef1b2396af
José Carvalho <z.m.f.carvalho@gmail.com>
2022-04-11 20:36:25 +0100

    Changing Report

--- src/algorithms/thirdScenario.cpp
+++ src/algorithms/thirdScenario.cpp
@@ -17,44 +17,49 @@ thirdScenario::thirdScenario(const vector<request>& requests1, const vector<truc
     express.cost = trucks.at(0).cost;
 }
 
-
 void thirdScenario::compute() {
     for (int i = 0; i < requests.size();i++)
         if (isDeliverable(i))deliver(requests.at(i));
-
     printStats();
 }
 
 void thirdScenario::printStats(){
     auto deliPerc = (double)(((double)nReq / (double)requests.size()) * (double)100.0);
     double avgTime = usedTime/nReq;
+    int avgTimeM = avgTime/60;
+    int avgTimeS = int(avgTime)%60;
 
-    cout << "-*----------------------------------------------------*-" << endl;
+    cout << "-*-------------  Report Scenario 3  ------------------*-" << endl;
     cout << " |--> Truck Details: " << endl;
     cout << " |        Truck Maximum Weight: " <<express.maxWeight << "Kg" << endl;
     cout << " |        Truck Maximum Volume: " <<express.maxVolume << "m3" <<endl;
     cout << " |        Truck Cost: " <<express.cost << "$"<< endl;
     cout << " |--> Deliveries: " << endl;
 
-    cout << " |        Delivered: " << deliPerc << "%  (" <<nReq<<")"<< endl;
+    cout << " |        Delivered: " << deliPerc << "% ("<<nReq<<")"<< endl;
 
     if (requests.size() - nReq == 0){
-        cout << " |        Rejected 0%" << endl;
+        cout << " |        Rejected: 0%" << endl;
     }else
-        cout << " |        Rejected " << 100 - deliPerc << "%" << endl;
-    cout << " |        Average Time per Delivery: "<< ((double)avgTime/(double)60 +((int)avgTime % 60))<< " min -----" <<usedTime <<endl;
+        cout << " |        Rejected: " << 100 - deliPerc << "%" << endl;
+    cout << " |        Average Time per Delivery: "<< avgTimeM << ","<<avgTimeS<<" min"<<endl;
     cout << " |--> Balance: " << endl;
     cout << " |       Gains from Deliveries: " << income << "$" << endl;
-    cout << " |       Total profit " <<income - express.cost<< "$"<< endl;
+    cout << " |       Total profit: " <<income - express.cost<< "$"<< endl;
 
     cout << "-*----------------------------------------------------*-" << endl;
     cout << "    Do you want to see the request details? (y/n)" << endl;
     char answer;
     cin >> answer;
-    if(answer == 'y')
-        for (int i = 0 ; i < deliveryList.size(); i++)
-            cout << "[" <<i+1<<"] "<< deliveryList.at(i).weight<<"Kg "<< deliveryList.at(i).volume <<"m3 " << deliveryList.at(i).reward<<"$ "<<deliveryList.at(i).duration <<" s"<< endl;
-}
+    if(answer == 'y'){
+        cout << "[Id] [Weight] [Volume] [Reward] [Delivery time]"<< endl;
+        for (int i = 0 ; i < deliveryList.size(); i++){
+            cout << "[" <<i+1<<"] "<< deliveryList.at(i).weight<<"Kg "<< deliveryList.at(i).volume <<"m3 " << deliveryList.at(i).reward<<"$ "<<deliveryList.at(i).duration <<"s"<< endl;
+        }
+    }
+    cout << "------  End Of Scenario 3  -------"<< endl<< endl<< endl;
+    }
+
 
 bool thirdScenario::lDeliveryTime(const request &a, const request &b) {
     return  a.duration < b.duration;}
