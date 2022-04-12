29996d2b49a12c795e50cca67c59e4ed6ef0ac8f
Eduardo Correia <up201909914@fc.up.pt>
2022-04-12 16:18:14 +0100

    finished greedy algorithm

--- src/algorithms/secondScenario.cpp
+++ src/algorithms/secondScenario.cpp
@@ -2,11 +2,20 @@
 // Created by Eduardo Correia on 09/04/2022.
 //
 
-
-#include <iostream>
-#include <algorithm>
-#include <queue>
 #include "secondScenario.h"
+
+void secondScenario::compute() {
+    unsigned int results[3];
+    unsigned int maximumValue = getMax3(results[0] = computeByWeight(),results[1] = computeByVolume(),results[2] = computeByWeight());
+
+    printResult(maximumValue);
+}
+
+secondScenario::secondScenario(const vector<request>& requests, const vector<truck>& trucks) : abstractAlgorithm(requests, trucks) {
+    sortedRequests = requests;
+    sortedTrucks = trucks;
+    bestScenario = 0;
+}
 
 void secondScenario::printTrucks(const vector<truck> &trucks) {
     for (const auto& truck: trucks) {
@@ -29,31 +38,29 @@ bool secondScenario::truckWeightToCost(const truck &a, const truck &b){
     return ((double)a.maxWeight/a.cost) > ((double)b.maxWeight/b.cost);
 }
 
-//Sort requests by biggest weight to reward ratio
+//Sort requests by biggest volume to reward ratio
 bool secondScenario::requestVolumeToReward(const request &a, const request &b){
     return ((double)a.volume*a.reward) > ((double)b.volume*b.reward);
 }
 
-//Sort trucks by biggest weight to cost ratio
+//Sort trucks by biggest volume to cost ratio
 bool secondScenario::truckVolumeToCost(const truck &a, const truck &b){
     return ((double)a.maxVolume/a.cost) > ((double)b.maxVolume/b.cost);
 }
 
-secondScenario::secondScenario(const vector<request>& requests, const vector<truck>& trucks) : abstractAlgorithm(requests, trucks) {
-    sortedRequests = requests;
-    sortedTrucks = trucks;
+//Sort requests by biggest V*W to reward ratio
+bool secondScenario::wvRatioToReward(const request &a, const request &b){
+    return ((double)a.volume*a.weight*a.reward) > ((double)b.volume*b.weight*b.reward);
+}
+
+//Sort trucks by biggest V*W to cost ratio
+bool secondScenario::wvRatioToCost(const truck &a, const truck &b){
+    return ((double)(a.maxVolume*a.maxVolume)/a.cost) > ((double)(b.maxVolume*b.maxWeight)/b.cost);
 }
 
 
-void secondScenario::compute() {
-    cout << "!----------------------!" << endl;
-    cout << computeByWeight() << endl;
-    cout << computeByVolume() << endl;
-    cout << "!----------------------!" << endl;
-}
 
-
-int secondScenario::computeByWeight() {
+unsigned int secondScenario::computeByWeight() {
 
     std::queue<request> requestQueue;
     unsigned int truckIndex = 0;
@@ -61,7 +68,7 @@ int secondScenario::computeByWeight() {
     //Sort requests by weight * reward (higher first), and sort trucks by weight/cost (higher first)
     std::sort(sortedRequests.begin(), sortedRequests.end(), requestWeightToReward);
     std::sort(sortedTrucks.begin(), sortedTrucks.end(), truckWeightToCost);
-    weightOutput = sortedTrucks;
+    outputTrucks[0] = sortedTrucks;
 
     //Insert requests into a request queue
     for(auto request : sortedRequests)
@@ -69,14 +76,14 @@ int secondScenario::computeByWeight() {
 
     while(!requestQueue.empty()){
         request currentRequest = requestQueue.front();
-        if(!checkAndInsert(currentRequest,weightOutput.at(truckIndex))){
+        if(!checkAndInsert(currentRequest,outputTrucks[0].at(truckIndex))){
             truckIndex++;
-            if(truckIndex >= weightOutput.size()) break;
+            if(truckIndex >= outputTrucks[0].size()) break;
         }
         requestQueue.pop();
     }
 
-    return calculateProfit(weightOutput);
+    return calculateProfit(outputTrucks[0]);
 
 }
 
@@ -98,7 +105,7 @@ unsigned int secondScenario::computeByVolume() {
     //Sort requests by weight * reward (higher first), and sort trucks by weight/cost (higher first)
     std::sort(sortedRequests.begin(), sortedRequests.end(), requestVolumeToReward);
     std::sort(sortedTrucks.begin(), sortedTrucks.end(), truckVolumeToCost);
-    volumeOutput = sortedTrucks;
+    outputTrucks[1] = sortedTrucks;
 
     //Insert requests into a request queue
     for(auto request : sortedRequests)
@@ -106,18 +113,39 @@ unsigned int secondScenario::computeByVolume() {
 
     while(!requestQueue.empty()){
         request currentRequest = requestQueue.front();
-        if(!checkAndInsert(currentRequest,volumeOutput.at(truckIndex))){
+        if(!checkAndInsert(currentRequest,outputTrucks[1].at(truckIndex))){
             truckIndex++;
-            if(truckIndex >= volumeOutput.size()) break;
+            if(truckIndex >= outputTrucks[1].size()) break;
         }
         requestQueue.pop();
     }
 
-    return calculateProfit(volumeOutput);
+    return calculateProfit(outputTrucks[1]);
 }
 
 unsigned int secondScenario::computeByWeightToVolume() {
-    return 0;
+    std::queue<request> requestQueue;
+    unsigned int truckIndex = 0;
+
+    //Sort requests by weight * reward (higher first), and sort trucks by weight/cost (higher first)
+    std::sort(sortedRequests.begin(), sortedRequests.end(), wvRatioToReward);
+    std::sort(sortedTrucks.begin(), sortedTrucks.end(), wvRatioToCost);
+    outputTrucks[2] = sortedTrucks;
+
+    //Insert requests into a request queue
+    for(auto request : sortedRequests)
+        requestQueue.push(request);
+
+    while(!requestQueue.empty()){
+        request currentRequest = requestQueue.front();
+        if(!checkAndInsert(currentRequest,outputTrucks[2].at(truckIndex))){
+            truckIndex++;
+            if(truckIndex >= outputTrucks[2].size()) break;
+        }
+        requestQueue.pop();
+    }
+
+    return calculateProfit(outputTrucks[2]);
 }
 
 int secondScenario::calculateProfit(vector<truck> &trucks) {
@@ -142,5 +170,49 @@ int secondScenario::calculateProfit(vector<truck> &trucks) {
 
     return profit;
 }
+
+unsigned int secondScenario::getMax3(unsigned int value1,unsigned int value2,unsigned int value3) {
+    unsigned int max = value1;
+    if(value2 > value1 && value2 > value3){
+        max = value2;
+        bestScenario = 1;
+    }
+    else if(value3 > value1 && value3 > value2){
+        max = value3;
+        bestScenario = 2;
+    }
+    return max;
+}
+
+unsigned int secondScenario::calculateCost(const vector<truck> &trucks) {
+    unsigned int cost = 0;
+    for(const auto& truck : trucks){
+        cost+= truck.cost;
+    }
+    return cost;
+}
+
+unsigned int secondScenario::numberOfRequests(const vector<truck> &trucks) {
+    unsigned int counter = 0;
+    for(const auto& truck : trucks){
+        counter += truck.truckLoad.size();
+    }
+    return counter;
+}
+
+void secondScenario::printResult(unsigned int maximumValue) {
+    cout << "-*-------------  Report Scenario 2  ------------------*-" << endl;
+    cout << " |--> Truck Details: " << endl;
+    cout << " |        Number of Trucks: " << trucks.size() << endl;
+    cout << " |        Percentage of Trucks used: " << (double)outputTrucks[bestScenario].size()/(double)trucks.size() * 100.0 << "%" << endl;
+    cout << " |--> Deliveries: " << endl;
+    cout << " |        Number of Deliveries: " << requests.size() << endl;
+    cout << " |        Delivered: " << ((double)numberOfRequests(outputTrucks[bestScenario])/(double)requests.size()) * 100.0 << "%" << endl;
+    cout << " |--> Balance: " << endl;
+    cout << " |        Truck Cost: " << calculateCost(outputTrucks[bestScenario]) << "$" << endl;
+    cout << " |        Total profit: " << maximumValue << "$" << endl;
+    cout << "-*----------------------------------------------------*-" << endl;
+}
+
 
 
--- src/algorithms/secondScenario.h
+++ src/algorithms/secondScenario.h
@@ -7,28 +7,39 @@
 
 
 #include "../abstractAlgorithm.h"
+#include <iostream>
+#include <algorithm>
+#include <queue>
 
 class secondScenario : abstractAlgorithm{
 private:
     vector<request> sortedRequests;
     vector<truck> sortedTrucks;
-    vector<truck> weightOutput;
-    vector<truck> volumeOutput;
-    vector<truck> weightToVolumeOutput;
+    vector<truck> outputTrucks[3];
+    int bestScenario;
     static bool truckWeightToCost(const truck &a, const truck &b);
     static bool requestWeightToReward(const request &a, const request &b);
     static bool truckVolumeToCost(const truck &a, const truck &b);
     static bool requestVolumeToReward(const request &a, const request &b);
+    static bool wvRatioToCost(const truck &a, const truck &b);
+    static bool wvRatioToReward(const request &a, const request &b);
     static void printTrucks(const vector<truck> &trucks);
     static void printRequests(const vector<request> &requests);
-    int computeByWeight();
+    unsigned int computeByWeight();
     unsigned int computeByVolume();
     unsigned int computeByWeightToVolume();
     static bool checkAndInsert(request requestToInsert, truck &truckToInsert);
     static int calculateProfit(vector<truck> &trucks);
+    unsigned int getMax3(unsigned int value1,unsigned int value2,unsigned int value3);
+    static unsigned int calculateCost(const vector<truck> &trucks);
+    static unsigned int numberOfRequests(const vector<truck> &trucks);
+    void printResult(unsigned int);
 public:
     secondScenario(const vector<request>& requests, const vector<truck>& trucks);
     void compute() override;
+
+
+
 };
 
 
